816b95e7af08d8921c0406372ff0a714beabd259
Vaishnavi <vaishnavin.cse2024@citchennai.net>
2026-02-07 12:40:24 +0530

    Add explicit publicDir to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import path from 'path';
+﻿import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       base: '/',
+      publicDir: 'public',
       server: {
         port: 3000,
         host: '0.0.0.0',
